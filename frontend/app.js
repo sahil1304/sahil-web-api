c56ffc4cb109b53e88432914ce363e0f89eb5f57
sahil1304 <78791911+sahil1304@users.noreply.github.com>
2021-02-12 22:05:30 +0530

    Add files via upload

---
+++ frontend/app.js
@@ -0,0 +1,36 @@
+const form = document.querySelector('form');
+const searchInput = document.querySelector('input');
+const resultsList = document.querySelector('#results')
+
+const BASE_URL = 'http://localhost:5000/api/';
+
+form.addEventListener('submit', formSubmitted);
+
+function formSubmitted(event) {
+  event.preventDefault();
+
+  const searchTerm = searchInput.value;
+  getSearchResults(searchTerm)
+    .then(showResults);
+}
+
+function getSearchResults(searchTerm) {
+  return fetch(`${BASE_URL}search/${searchTerm}`)
+    .then(res => res.json());
+}
+
+function showResults(results) {
+  console.log(results);
+	resultsList.innerHTML="";
+  results.forEach(movie => {
+    const li = document.createElement('li');
+    const img = document.createElement('img');
+    li.appendChild(img);
+    img.src = movie.image;
+    const a = document.createElement('a');
+    a.textContent = movie.title;
+    a.href = '/movie.html?imdbID=' + movie.imdbID;
+    li.appendChild(a);
+    resultsList.appendChild(li);
+  });
+}
---
+++ frontend/movie.js
@@ -0,0 +1,59 @@
+const main = document.querySelector('main');
+const imdbID = window.location.search.match(/imdbID=(.*)/)[1];
+const BASE_URL = 'http://localhost:5000/api/';
+function getMovie(imdbID) {
+  return fetch(`${BASE_URL}movie/${imdbID}`)
+    .then(res => res.json());
+}
+
+function showMovie(movie) {
+ 
+  console.log(movie);
+  const section = document.createElement('section');
+  main.appendChild(section);
+
+
+  const date = dateFns.parse(movie.datePublished);
+  movie.datePublished = dateFns.format(date, 'MMMM Do YYYY');
+
+  const properties = [{
+    title: 'Rating',
+    property: 'rating'
+  }, {
+    title: 'Run Time',
+    property: 'runTime'
+  }, {
+    title: 'Released',
+    property: 'datePublished'
+  }, {
+    title: 'Summary',
+    property: 'summary'
+  }, {
+    title: 'Story Line',
+    property: 'storyLine'
+  }];
+
+  const descriptionHTML = properties.reduce((html, property) => {
+    html += `
+      <dt class="col-sm-3">${property.title}</dt>
+      <dd class="col-sm-9">${movie[property.property]}</dd>`;
+    return html;
+  }, '');
+
+  section.outerHTML = `
+    <section class="row" style="padding-top:20px;">
+      <h1 class="text-center">${movie.title}</h1>
+      <div class="col-sm-12">
+        <img src="${movie.poster}" class="img-fluid" height=30px; width=30px; />
+      </div>
+      <div class="col-sm-12">
+        <dl class="row">
+          ${descriptionHTML}
+        </dl>
+      </div>
+    </section>
+  `;
+}
+
+getMovie(imdbID)
+  .then(showMovie);
